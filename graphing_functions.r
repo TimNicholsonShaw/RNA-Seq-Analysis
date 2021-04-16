d8cf99a18d3d847771f306793b8ecd929b85de04
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-04-16 12:15:30 -0700

    It does PCA plots now

--- graphing_functions.r
+++ graphing_functions.r
@@ -1,7 +1,24 @@
 # Functions for making cool graphs of RNA Seq data
 
-plot_pca <- function(DESeq_object, intgroup) {
+# imports
+library(ggplot2)
 
+plot_pca <- function(DESeq_object, metadata) {
+    # Taken from UCSD BIOM262 2017
+    # This only works if it has a condition and batch
+    # One day I will code this to be more flexible
+    rld <- rlog(DESeq_object)
+
+    data <- plotPCA(rld, intgroup="condition", returnData=TRUE)
+    data$batch <- as.factor(metadata$batch)
+
+    percentVar <- round(100 * attr(data, "percentVar"))
+    p <- ggplot(data, aes(PC1, PC2, color=condition, shape=batch)) +
+            geom_point(size=3) +
+            xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+            ylab(paste0("PC2: ",percentVar[2],"% variance"))
+
+    return(p)
 }
 
 plot_sample_distances <- function(DESeq_object) {
