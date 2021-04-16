20c230b1834bdac1d80b522cc8d447fc2db58f15
Tim Nicholson-Shaw <timnicholsonshaw@gmail.com>
2021-04-16 10:47:43 -0700

    Can read in genesig and metadata

--- graphing_functions.r
+++ graphing_functions.r
@@ -9,5 +9,10 @@ plot_sample_distances <- function(DESeq_object) {
 }
 
 plot_volcano <- function(results_table) {
-    
+
 }
+
+plot_volcano_facet_gene_type <- function(results_table, gene_type_vector) {
+
+}
+
--- processing_functions.r
+++ processing_functions.r
@@ -4,13 +4,27 @@
 
 #imports
 library(DESeq2)
+library(tidyverse)
 
+make_genesig_from_file <- function (file_loc, ...) {
+    # extra arguments should contain mappings for changing names
+    # will likely only work for featureCounts output
+    genesig_df <- read_tsv(file_loc, col_names=T, comment="#")
+    genesig_df <- rename(genesig_df, ...)
 
-make_genesig_from_file <- function (file_loc, sample_names_in_order) {
+    # drop unused columns
+    # still hang onto length, because we can use it in TPM calculations
+    # but that would need to be dropped before DESeq
+    return(select(genesig_df, -Chr, -Start, -End, -Strand)) 
 }
 
-make_meta_data <- function(genesig_df, some_mapping_of_conditions) {
+# should end with a mapping of conditions that matches the order of the column names
+make_meta_data <- function(genesig_df, ...) {
+    # Gets column names from  genesig_df
+    # Drops geneid and length
+    sample_names <- colnames(genesig_df[-(1:2)])
 
+    return(data.frame(Sample=sample_names, ...))
 }
 
 make_DESeq_object <- function(genesig_df, meta_data, design) {
@@ -18,5 +32,5 @@ make_DESeq_object <- function(genesig_df, meta_data, design) {
 }
 
 make_results_table <- function(DESeq_object, contrast_vector) {
-    
+
 }
